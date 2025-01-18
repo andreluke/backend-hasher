270653e1208cb1f391117df3ba6d8407965504f5
André Lucas <deco99885@gmail.com>
2025-01-18 15:09:35 -0300

    Update link.js

--- build/routes/link.js
+++ build/routes/link.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import controller from "../controllers/LinkController.js";
 const routes = Router();
-routes.get("/", controller.list);
+// routes.get("/", controller.list);
 routes.get("/:slug", controller.getBySlug);
 routes.post("/", controller.create);
 routes.delete("/:id", controller.delete);
